4a6b7dd46fa55f61ac50e3582ee1273d1a55d237
Triangle-123 <83333122+Triangle-123@users.noreply.github.com>
2024-08-07 17:54:06 +0900

    [Bronze V] Title: 자동완성, Time: 88 ms, Memory: 12912 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		String n = sc.next();
+		if(n.equals("N") || n.equals("n")) System.out.println("Naver D2");
+		else System.out.println("Naver Whale");
+		
+	}
+}
