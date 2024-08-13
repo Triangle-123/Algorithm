73f33d7ee5fbc096fb832b64a0d049bf06418668
Triangle-123 <83333122+Triangle-123@users.noreply.github.com>
2024-08-13 11:41:12 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 112 ms, Memory: 18,364 KB -BaekjoonHub

---
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		List<Integer> list = new ArrayList<>();
+		Stack<Integer> stack = new Stack<>();
+		
+		for(int t = 1; t <= 10; t++) {
+			int n = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < n; i++) {
+				list.add(Integer.parseInt(st.nextToken()));
+			}
+			int command = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < command; i++) {
+				if(st.nextToken().equals("I")) {
+					int idx = Integer.parseInt(st.nextToken());
+					int nums = Integer.parseInt(st.nextToken());
+					
+					for(int j = 0; j < nums; j++) {
+						stack.push(Integer.parseInt(st.nextToken()));
+					}
+					
+					while(!stack.isEmpty()) {
+						list.add(idx, stack.pop());
+					}
+					
+				}
+			}
+			
+			System.out.print("#" + t + " ");
+			for(int i = 0; i < 10; i++) {
+				System.out.print(list.get(i) + " "); 
+			}
+			System.out.println();
+			list.clear();
+		}
+	}
+}
