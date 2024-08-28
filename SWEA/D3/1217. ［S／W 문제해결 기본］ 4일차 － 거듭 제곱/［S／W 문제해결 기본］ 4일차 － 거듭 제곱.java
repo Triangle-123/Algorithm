24d95bb659da440653ff886fad748570b1161d91
Triangle-123 <83333122+Triangle-123@users.noreply.github.com>
2024-08-28 20:09:20 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 149 ms, Memory: 20,496 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -3,21 +3,26 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		for(int t = 1; t <= 10; t++) {
 			int tCase = sc.nextInt();
 			int n = sc.nextInt();
 			int m = sc.nextInt();
 			
-			int result = recursive(n, m);
-			System.out.printf("#%d %d\n", tCase, result);
+			int result = pow(n, m);
+			System.out.println("#" + tCase + " " + result);
 		}
-		
-		sc.close();
 	}
-	
-	static int recursive(int n, int m) {
-		if(m == 1) return n;
-		return n * recursive(n, m - 1);
+
+	private static int pow(int n, int m) {
+		
+		if(m == 0) return 1;
+		if(m % 2 == 0) {
+			int result = pow(n, m / 2);
+			return result * result;
+		} else {
+			int result = pow(n, (m - 1) / 2);
+			return result * result * n;
+		}
+
 	}
 }
