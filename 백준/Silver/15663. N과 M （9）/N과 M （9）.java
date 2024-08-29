3e7e6d5bfa548c9815ab1363e090bb315ee063f4
Triangle-123 <83333122+Triangle-123@users.noreply.github.com>
2024-08-30 01:23:26 +0900

    [Silver II] Title: N과 M (9), Time: 188 ms, Memory: 37356 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,11 +1,5 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Main {
 	
@@ -13,7 +7,6 @@ public class Main {
 	static int[] sel;
 	static boolean[] visited;
 	static int[] data;
-	static Set<String> compare = new HashSet<>();
 	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) {
@@ -31,41 +24,24 @@ public class Main {
 		}
 		Arrays.sort(data);
 		perm(0);
-		List<String> list = new ArrayList<>(compare);
-		Collections.sort(list, new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				String[] s1 = o1.split(" ");
-				String[] s2 = o2.split(" ");
-				
-				for(int i = 0; i < s1.length; i++) {
-					if(Integer.parseInt(s1[i]) == Integer.parseInt(s2[i])) continue;
-					return Integer.parseInt(s1[i]) - Integer.parseInt(s2[i]);
-				}
-				return 0;
-			}
-		});
-		for(int i = 0; i < list.size(); i++) {
-			sb.append(list.get(i));
-		}
 		System.out.println(sb);
 	}
 
 	private static void perm(int idx) {
 		if(idx == M) {
-			StringBuilder sb2 = new StringBuilder();
 			for(int i = 0; i < sel.length; i++) {
-				sb2.append(sel[i] + " ");
+				sb.append(sel[i] + " ");
 			}
-			sb2.append("\n");
-			compare.add(sb2.toString());
+			sb.append("\n");
 
 			return;
 		}
 		
+		int before = 0;
 		for(int i = 0; i < N; i++) {
-			if(!visited[i]) {
+			if(!visited[i] && before != data[i]) {
 				visited[i] = true;
+				before = data[i];
 				sel[idx] = data[i];
 				perm(idx + 1);
 				visited[i] = false;
