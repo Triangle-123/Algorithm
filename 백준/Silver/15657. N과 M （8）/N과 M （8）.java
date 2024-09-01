5003acf7f8ce532f40424dd973d9b5d644079fa9
Triangle-123 <83333122+Triangle-123@users.noreply.github.com>
2024-09-01 15:33:42 +0900

    [Silver III] Title: N과 M (8), Time: 136 ms, Memory: 24508 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	
+	static int N, M;
+	static int[] data;
+	static int[] sel;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		data = new int[N];
+		sel = new int[M];
+		
+		for(int i = 0; i < N; i++) {
+			data[i] = sc.nextInt();
+		}
+		Arrays.sort(data);
+		
+		combi(0, 0);
+		System.out.println(sb);
+	}
+
+	private static void combi(int idx, int sidx) {
+		if(sidx == M) {
+			for(int i = 0; i < sel.length; i++) {
+				sb.append(sel[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		if(idx == N) return;
+		
+		sel[sidx] = data[idx];
+		combi(idx, sidx + 1);
+		combi(idx + 1, sidx);
+		
+	}
+}
