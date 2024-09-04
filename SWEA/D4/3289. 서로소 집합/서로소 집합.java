fbcce37c1e0ae004692c193dcb7513e9258e7d3b
Triangle-123 <83333122+Triangle-123@users.noreply.github.com>
2024-09-04 11:41:29 +0900

    [D4] Title: 서로소 집합, Time: 1,591 ms, Memory: 129,856 KB -BaekjoonHub

--- SWEA/D4/3289. 서로소 집합/서로소 집합.java
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -31,18 +31,23 @@ public class Solution {
 		}
 	}
 
-	private static void union(int x, int y) {
-		parents[findSet(y)] = findSet(x);
+	private static boolean union(int x, int y) { // 합치는 연산 (y가 x를 가리키도록)
+//		parents[findSet(y)] = findSet(x);
 		
+		int xRoot = findSet(x);  // 크루스칼 알고리즘의 사이클 체크(union에 find를 넣으면 되는구나)
+		int yRoot = findSet(y);
+		if(xRoot == yRoot) return false;
+		parents[yRoot] = xRoot;
+		return true;
 	}
 
-	private static void makeSet() {
+	private static void makeSet() { // 서로소 집합 만들기
 		for(int i = 1; i <= N; i++) {
 			parents[i] = i;
 		}
 	}
 	
-	private static int findSet(int x) {
+	private static int findSet(int x) { // Pass Compression 방식의 find (노드 정보 갱신으로 같은 연산 반복할 때 바로 반환) 
 		if(x != parents[x]) {
 			parents[x] = findSet(parents[x]);
 		}
