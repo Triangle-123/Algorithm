492966ea506893798d4c122eab47b837f34c1e38
Triangle-123 <83333122+Triangle-123@users.noreply.github.com>
2024-09-10 21:45:43 +0900

    [Gold III] Title: 파티, Time: 760 ms, Memory: 62628 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+	
+	static class Node {
+		int V, W;
+		
+		public Node(int v, int w) {
+			V = v;
+			W = w;
+		}
+	}
+	
+	static List<Node>[] graph;
+	static int N, M, X;
+	static int[] dist;
+	static boolean[] visited;
+	static PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.W - o2.W);
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		X = sc.nextInt();
+		
+		graph = new ArrayList[N + 1];
+		for(int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		dist = new int[N + 1];
+		visited = new boolean[N + 1];
+		
+		for(int i = 0; i < M; i++) {
+			graph[sc.nextInt()].add(new Node(sc.nextInt(), sc.nextInt()));
+		}
+		
+		int[] student = new int[N + 1];
+		for(int i = 1; i <= N; i++) {
+			dijkstra(i);
+			Arrays.fill(visited, false);
+			student[i] += dist[X];
+		}
+		
+		dijkstra(X);
+		int max = 0;
+		for(int i = 1; i <= N; i++) {
+			student[i] += dist[i];
+			max = Math.max(max, student[i]);
+		}
+		System.out.println(max);
+	}
+
+	private static void dijkstra(int start) {
+		for(int i = 1; i <= N; i++) {
+			dist[i] = Integer.MAX_VALUE;
+		}
+		dist[start] = 0;
+		pq.offer(new Node(start, 0));
+		
+		while(!pq.isEmpty()) {
+			Node node = pq.poll();
+			if(visited[node.V]) continue;
+			visited[node.V] = true;
+			for(Node next : graph[node.V]) {
+				if(!visited[next.V] && dist[next.V] > dist[node.V] + next.W) {
+					dist[next.V] = dist[node.V] + next.W;
+					pq.offer(new Node(next.V, dist[next.V]));
+				}
+			}
+		}
+		
+	}
+}
