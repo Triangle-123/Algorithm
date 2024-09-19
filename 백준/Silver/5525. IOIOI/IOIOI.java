6c6c2de2b5c3c85954f76e5c0b929db49447a6e8
Triangle-123 <83333122+Triangle-123@users.noreply.github.com>
2024-09-20 02:30:00 +0900

    [Silver I] Title: IOIOI, Time: 160 ms, Memory: 20612 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -7,33 +7,19 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		int M = Integer.parseInt(br.readLine());
 		String text = br.readLine();
-		String pattern = "I";
-		for(int i = 0; i < N; i++) {
-			pattern += "OI";
-		}
-		int[] skip = new int[Character.MAX_VALUE + 1];
-		for(int i = 0; i < Character.MAX_VALUE; i++) {
-			skip[i] = pattern.length();
-		}
-		for(int i = 0; i < pattern.length() - 1; i++) {
-			skip[pattern.charAt(i)] = pattern.length() - 1 - i;
-		}
-		
 		int cnt = 0;
-		int tPoint = pattern.length() - 1;
-		while(tPoint < text.length()) {
-			int pPoint = pattern.length() - 1;
-			
-			while(text.charAt(tPoint) == pattern.charAt(pPoint)) {
-				if(pPoint == 0) {
-					cnt++;
-					break;
+		int ans = 0;
+		for(int i = 1; i < M - 1; i++) {
+			if(text.charAt(i - 1) == 'I' && text.charAt(i) == 'O' && text.charAt(i + 1) == 'I') {
+				cnt++;
+				if(cnt == N) {
+					cnt--;
+					ans++;
 				}
-				pPoint--;
-				tPoint--;
-			}
-			tPoint += Math.max(skip[text.charAt(tPoint)], pattern.length() - pPoint);
+				i++;
+			} else cnt = 0;
+			
 		}
-		System.out.println(cnt);
+		System.out.println(ans);
 	}
 }
