fcf6d806708fdf6aa2f4271d96ec338f839555c2
Triangle-123 <83333122+Triangle-123@users.noreply.github.com>
2024-09-29 06:36:00 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 160 ms, Memory: 24,204 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,41 +1,32 @@
 import java.util.Scanner;
 
 public class Solution {
-	
-	static int[] price = new int[4];
-	static int[] plan = new int[12];
-	static int minPrice;
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int tCase = sc.nextInt();
 		for(int t = 1; t <= tCase; t++) {
-			
+			int[] price = new int[4];
 			for(int i = 0; i < 4; i++) {
 				price[i] = sc.nextInt();
 			}
-			
-			minPrice = price[3];
-			
-			for(int i = 0; i < 12; i++) {
+			int[] plan = new int[14];
+			for(int i = 1; i <= 12; i++) {
 				plan[i] = sc.nextInt();
 			}
+			int[][] dp = new int[14][3];
+			for(int i = 12; i >= 1; i--) {
+				
+				dp[i][0] = plan[i] * price[0];
+				dp[i][1] = price[1];
+				dp[i][2] = price[2];
 			
-			DFS(0, 0);
-			System.out.println("#" + t + " " + minPrice);
+				if(i <= 9) dp[i][2] += Math.min(Math.min(dp[i + 3][0], dp[i + 3][1]), dp[i + 3][2]);
+				dp[i][0] += Math.min(Math.min(dp[i + 1][0], dp[i + 1][1]), dp[i + 1][2]);
+				dp[i][1] += Math.min(Math.min(dp[i + 1][0], dp[i + 1][1]), dp[i + 1][2]);
+			}
+			
+			int ans = Math.min(Math.min(dp[1][0], dp[1][1]), Math.min(dp[1][2], price[3]));
+			System.out.println("#" + t + " " + ans);
 		}
-	}
-
-	private static void DFS(int curr, int idx) {
-		
-		for(int i = idx; i < 12; i++) {
-			if(plan[i] == 0) continue;
-			DFS(curr + plan[i] * price[0], i + 1);
-			DFS(curr + price[1], i + 1);
-			DFS(curr + price[2], i + 3);
-			return;
-		}
-		minPrice = Math.min(curr, minPrice);
-		
 	}
 }
