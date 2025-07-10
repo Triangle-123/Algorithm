cecd35196105abcb44c0942f2e5be445c6f0c573
Jeongpyo Hong <hsc3757@gmail.com>
2025-07-10 16:19:25 +0900

    [Bronze V] Title: 기찍 N, Time: 168 ms, Memory: 29440 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -6,9 +6,11 @@ class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for(int i = N; i > 0; i--) {
-            System.out.println(i);
+            sb.append(i + "\n");
         }
+        System.out.println(sb);
     }
     
 }
