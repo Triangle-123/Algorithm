cf058042d7dda72dabdddb754cdfb8fbe00872b1
Jeongpyo Hong <hsc3757@gmail.com>
2025-07-21 19:50:18 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 128 ms, Memory: 17296 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,63 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 	
-	static class Node implements Comparable<Node> {
-		int vertex, cost;
-
-		public Node(int vertex, int cost) {
-			this.vertex = vertex;
-			this.cost = cost;
-		}
-
-		@Override
-		public int compareTo(Node o) {
-			return this.cost - o.cost;
-		}
+	static int time = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		BFS(N, K);
+		System.out.println(time);
 	}
 
-	static int[] dist;
-	
-	private static void dijkstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		dist[start] = 0;
-		pq.offer(new Node(start, 0));
-		while (!pq.isEmpty()) {
-			Node cur = pq.poll();
-			if (dist[cur.vertex] < cur.cost) continue;
-			
-			// 순간이동
-			if (cur.vertex * 2 <= 100000 && dist[cur.vertex * 2] > dist[cur.vertex]) {
-				dist[cur.vertex * 2] = dist[cur.vertex];
-				pq.offer(new Node(cur.vertex * 2, dist[cur.vertex]));
-			}
-			
-			// 걷기
-			if (cur.vertex + 1 <= 100000 && dist[cur.vertex + 1] > dist[cur.vertex] + 1) {
-				dist[cur.vertex + 1] = dist[cur.vertex] + 1;
-				pq.offer(new Node(cur.vertex + 1, dist[cur.vertex] + 1));
-			}
-			if (cur.vertex - 1 >= 0 && dist[cur.vertex - 1] > dist[cur.vertex] + 1) {
-				dist[cur.vertex - 1] = dist[cur.vertex] + 1;
-				pq.offer(new Node(cur.vertex - 1, dist[cur.vertex] + 1));
+	private static void BFS(int n, int k) {
+		Queue<Integer> q = new ArrayDeque<>();
+		q.offer(n);
+		int[] visited = new int[200001];
+		Arrays.fill(visited, -1);;
+		visited[n] = 0;
+		while(!q.isEmpty()) {
+			int qSize = q.size();
+			for(int s = 0; s < qSize; s++) {
+				int x = q.poll();
+				if(x == k) {
+					time = Math.min(time, visited[x]);
+				}
+				int front = x + 1;
+				int back = x - 1;
+				int teleport = x * 2;
+				if(front <= 100000 && (visited[front] == -1 || visited[front] > visited[x] + 1)) {
+					q.offer(front);
+					visited[front] = visited[x] + 1;
+				}
+				if(back >= 0 && (visited[back] == -1 || visited[back] > visited[x] + 1)) {
+					q.offer(back);
+					visited[back] = visited[x] + 1;
+				}
+				if(teleport <= 200000 && (visited[teleport] == -1 || visited[teleport] > visited[x])) {
+					q.offer(teleport);
+					visited[teleport] = visited[x];
+				}
 			}
 		}
 	}
-	
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        dist = new int[100001];
-        Arrays.fill(dist, 999999);
-        
-        dijkstra(N);
-        System.out.println(dist[K]);
-    }
 }
